86b310d3c3dfc57dc2ef696d279bb07249b2a9ea
Anastasia <anastasiia.tsyhanova@ibvcloud.com>
2024-02-27 16:39:28 +0200

    New commit

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
 document.querySelectorAll('.elementSection').forEach(item => {
     item.addEventListener('click', function() {
-      const plus = this.querySelector('.elementPlus');
-      if (plus) {
-        plus.classList.toggle('active');
-      }
-      
-      const content = this.nextElementSibling;
-      if (content.classList.contains('hidden')) {
-        content.classList.remove('hidden');
-        setTimeout(() => content.style.height = content.scrollHeight + "px", 0);
-      } else {
-        content.style.height = "0px";
-        setTimeout(() => content.classList.add('hidden'), 150);
-      }
+        document.querySelectorAll('.elementSection').forEach(otherItem => {
+            if (otherItem !== item) {
+                const otherPlus = otherItem.querySelector('.elementPlus');
+                if (otherPlus && otherPlus.classList.contains('active')) {
+                    otherPlus.classList.remove('active');
+                }
+                const otherContent = otherItem.nextElementSibling;
+                if (!otherContent.classList.contains('hidden')) {
+                    otherContent.style.height = "0px";
+                    setTimeout(() => otherContent.classList.add('hidden'), 150);
+                }
+            }
+        });
+
+        const plus = item.querySelector('.elementPlus');
+        if (plus) {
+            plus.classList.toggle('active');
+        }
+
+        const content = item.nextElementSibling;
+        if (content.classList.contains('hidden')) {
+            content.classList.remove('hidden');
+            setTimeout(() => content.style.height = content.scrollHeight + "px", 0);
+        } else {
+            content.style.height = "0px";
+            setTimeout(() => content.classList.add('hidden'), 150);
+        }
     });
-  });
-  
+});
+
+// document.querySelectorAll('.elementSection').forEach(item => {
+//     item.addEventListener('click', function() {
+//       const plus = this.querySelector('.elementPlus');
+//       if (plus) {
+//         plus.classList.toggle('active');
+//       }
+      
+//       const content = this.nextElementSibling;
+//       if (content.classList.contains('hidden')) {
+//         content.classList.remove('hidden');
+//         setTimeout(() => content.style.height = content.scrollHeight + "px", 0);
+//       } else {
+//         content.style.height = "0px";
+//         setTimeout(() => content.classList.add('hidden'), 150);
+//       }
+//     });
+//   });
